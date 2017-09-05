a73f1b9bdb568780d69c36877de763cc1e95841a
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2017-09-05 04:23:30 -0700

    remove value property from disabled select menu option following 'none' option will be selected instead

--- src/components/Book.js
+++ src/components/Book.js
@@ -19,8 +19,8 @@ class Book extends Component {
         <div className="book-top">
           <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: "url(" + thumbnail + ")" }}></div>
           <div className="book-shelf-changer">
-            <select value={book.shelf ? book.shelf : "none"} onChange={(e) => this.props.updateReadingStatus(book, e.target.value)} >
-              <option value="none" disabled>Move to...</option>
+            <select value={book.hasOwnProperty('shelf') ? book.shelf : "none"} onChange={(e) => this.props.updateReadingStatus(book, e.target.value)} >
+              <option disabled>Move to...</option>
               <option value="currentlyReading">Currently Reading</option>
               <option value="wantToRead">Want to Read</option>
               <option value="read">Read</option>

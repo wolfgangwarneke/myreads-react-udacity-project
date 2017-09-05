86aa142af1bb6438bdc4378e366521c47a2a4966
Wolfgang Warneke <wolfgang.warneke@gmail.com>
2017-09-04 19:15:54 -0700

    add Link on home 'add button' to go to /search

--- src/components/BookShelf.js
+++ src/components/BookShelf.js
@@ -1,6 +1,6 @@
 import React, { Component, Children } from 'react'
 import Book from './Book'
-// import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 class BookShelf extends Component {
   render() {
@@ -40,6 +40,9 @@ class BookShelf extends Component {
               ))}
             </ol>
           </div>
+        </div>
+        <div className="open-search">
+        <Link to="/search">Add a book</Link>
         </div>
       </div>
 
